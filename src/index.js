9c6f7dd9b19a231372c85a3ef186df77445bae0c
Your Name <your.email@example.com>
2024-09-04 22:37:37 -0700

    firts commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h2>Lets try animation</h2>
+    <div id="animation"></div>
+    <div id="animation1"></div>
+    <div id="animation2"></div>
+    <div id="animation3"></div>
   </React.StrictMode>
 );
 
